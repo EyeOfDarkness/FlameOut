978003c71c2bcda73005f21c39f1de8ff9b0c7c6
EyeOfDarkness <64228877+EyeOfDarkness@users.noreply.github.com>
2024-01-11 20:34:29 +0800

    h

--- src/flame/FlameOut.java
+++ src/flame/FlameOut.java
@@ -24,7 +24,7 @@ public class FlameOut extends Mod{
     public static VaporizeBatch vaporBatch;
     public static DevastationBatch devasBatch;
 
-    public final boolean test = true;
+    public final boolean test = false;
 
     public FlameOut(){
         ApathyAoEBulletType.loadStatic();
